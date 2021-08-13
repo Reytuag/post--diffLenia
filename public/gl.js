c340d95d156eae258d6144b851c42c7a6dc85bcd
gautier HAMON <gautier.hamon@telecom-paristech.fr>
2021-08-13 19:13:32 +0200

    small fix

--- public/gl.js
+++ public/gl.js
@@ -445,7 +445,7 @@ function onResize(e) {
 
 
 var playButton=document.getElementById("play-pause");
-playButton.onlick(){
+playButton.onlick()= ()=>{
   pause = ! pause;
   if(pause){
     document.getElementById("play").style.display="none";
